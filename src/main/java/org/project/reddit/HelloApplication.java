b360c8f08f7d1dc5235cd4c4802dc1b12784b087
Amin Ghoorchian <mo.ghoorchian@gmail.com>
2024-03-28 18:20:52 +0330

    main files formatted

--- src/main/java/org/project/reddit/HelloApplication.java
+++ src/main/java/org/project/reddit/HelloApplication.java
@@ -8,6 +8,10 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
@@ -15,9 +19,5 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
-    }
-
-    public static void main(String[] args) {
-        launch();
     }
 }
